6e278642ce3826928f789e4d0590ff9a27e2f403
unknown <mariuszrokicki93@gmail.com>
2019-08-06 00:06:35 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,211 @@
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > 150) {
+//     document.querySelector('#main-nav').style.opacity = 0.9;
+//   } else {
+//     document.querySelector('#main-nav').style.opacity = 1;
+//   }
+// });
+
+
+// $(document).ready(function () {
+//   $('#testimonials').slick({
+//     dots: true
+
+
+//   });
+// });
+
+
+//mobile menu pop-up
+/* Mobile navigation */
+
+$('.hamburger-icon').click(function () {
+  var nav = $('.mobile-menu');
+  nav.slideToggle(200);
+  var icont = $('.hamburger-icon');
+  icont.removeClass('menu');
+
+});
+
+
+
+
+// map init
+
+function initMap() {
+
+  const loc = {
+    lat: 51.509,
+    lng: -0.118
+  };
+
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc,
+    styles: [{
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#e9e9e9"
+          },
+          {
+            "lightness": 17
+          }
+        ]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#f5f5f5"
+          },
+          {
+            "lightness": 20
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 17
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 29
+          },
+          {
+            "weight": 0.2
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 18
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 16
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#f5f5f5"
+          },
+          {
+            "lightness": 21
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#dedede"
+          },
+          {
+            "lightness": 21
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+            "visibility": "on"
+          },
+          {
+            "color": "#ffffff"
+          },
+          {
+            "lightness": 16
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [{
+            "saturation": 36
+          },
+          {
+            "color": "#333333"
+          },
+          {
+            "lightness": 40
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#f2f2f2"
+          },
+          {
+            "lightness": 19
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#fefefe"
+          },
+          {
+            "lightness": 20
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#fefefe"
+          },
+          {
+            "lightness": 17
+          },
+          {
+            "weight": 1.2
+          }
+        ]
+      }
+    ]
+  });
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map,
+    icon: '/dist/images/updated/marker.png'
+  });
+}
